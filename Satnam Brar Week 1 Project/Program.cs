ddf1f6894617fcd6d330c98ee669b472dc871144
Satnam Brar <satnamsbrar@gmail.com>
2017-01-07 00:19:22 -0500

    added code to prompt user for name

--- Satnam Brar Week 1 Project/Program.cs
+++ Satnam Brar Week 1 Project/Program.cs
@@ -10,6 +10,23 @@ namespace Satnam_Brar_Week_1_Project
     {
         static void Main(string[] args)
         {
+            //This code will prompt user to enter first name
+            //entry will be converted to lower case, followed by capitalization of the first letter
+            Console.WriteLine("Please enter your first name:\n");
+            string firstName = Console.ReadLine();
+            firstName = firstName.ToLower();
+            string firstName1 = firstName.Substring(0, 1);
+            firstName1 = firstName1.ToUpper();
+            firstName = firstName.Remove(0, 1);
+
+            //This code will prompt user to enter last name
+            //entry will be converted to lower case, followed by capitalization of the first letter
+            Console.WriteLine("Please enter your first name:\n");
+            string lastName = Console.ReadLine();
+            lastName = lastName.ToLower();
+            string lastName1 = lastName.Substring(0, 1);
+            lastName1 = lastName1.ToUpper();
+            lastName = lastName.Remove(0, 1);
         }
     }
 }
